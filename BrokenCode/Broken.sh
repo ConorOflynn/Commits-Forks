7b9853b42caa0599265fca10e4db0586224e2fc1
ConorOflynn <100319126+ConorOflynn@users.noreply.github.com>
2022-02-28 15:25:56 -0700

    Add files via upload

---
+++ BrokenCode/Broken.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+echo "Hello there what is your name?: "
+read $varname #added $
+echo "It is nice to meet you $varname"
+echo "What is your fav thing to do?"
+read $whatodo # added $
+echo "Oh thats pretty cool I like to $whatdodo also"
+echo "Okay goodbye have a great day (Leave a nice message for beepbot)"
+read input
+echo "oh you still here.... awkard"
